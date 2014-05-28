d91252b25dbafd6b0cd5358bfd5fe25cfca7eece
Goran Ehrsson <goran@technipelago.se>
2014-05-28 10:52:46 +0200

    Update and cleanup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,8 @@
-grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
+grails.servlet.version = "3.0"
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
@@ -10,19 +11,19 @@ grails.project.source.level = 1.6
 //grails.project.fork = [
 //   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
 //]
+// Increase heap and permgen in run-war mode
+grails.tomcat.jvmArgs = ["-Djava.awt.headless=true", "-Xms512m", "-Xmx768m", "-XX:MaxPermSize=320m"]
+grails.tomcat.nio = true
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
-    inherits("global") {
-        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
-        // excludes 'ehcache'
-    }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    checksums true // Whether to verify checksums on resolve
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+    inherits("global") {}
+    log "error"
+    checksums true
+    legacyResolve false
 
     repositories {
-        inherits true // Whether to inherit repository definitions from plugins
+        inherits true
 
         grailsPlugins()
         grailsHome()
@@ -30,34 +31,21 @@ grails.project.dependency.resolution = {
 
         mavenLocal()
         mavenCentral()
-
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
 
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-
-        // runtime 'mysql:mysql-connector-java:5.1.22'
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.8.3"
-        runtime ":resources:1.2.6"
-
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.5"
-
         build ":tomcat:$grailsVersion"
 
-        runtime ":database-migration:1.3.8"
-
         compile ':cache:1.1.1'
+
+        runtime ":hibernate:$grailsVersion"
+        runtime ":resources:1.2.7"
+        runtime ":jquery:1.11.1"
+        runtime ":twitter-bootstrap:2.3.2"
+        runtime ":less-resources:1.3.3.1"
+        runtime ":database-migration:1.3.6"
     }
 }
