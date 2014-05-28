74501efc55f336f1f94c349a67d6ec1abf0bfbd6
Goran Ehrsson <goran@technipelago.se>
2014-05-28 22:47:01 +0200

    Added Technipelago and Goran as demo contacts

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -17,10 +17,15 @@ class BootStrap {
             crmSecurityService.runAs(admin.username, tenant.id) {
                 crmSecurityService.addPermissionToUser("permission.all")
 
+                // Create some demo data.
                 def gr8conf = crmContactService.createCompany(name: "GR8Conf", email: "info@gr8conf.org",
                         address: [address1: "IT-University", city: 'Copenhagen', country: "DK"], true)
                 crmContactService.createPerson(firstName: "Søren", lastName: "Berg Glasius",
                         email: "sbglasius@gr8conf.org", parent: gr8conf, title: "Awesome Organizer", true)
+                def technipelago = crmContactService.createCompany(name: "Technipelago AB", email: "info@technipelago.se",
+                        website: 'www.technipelago.se', address: [city: 'Djurhamn', country: "SE"], true)
+                crmContactService.createPerson(firstName: "Göran", lastName: "Ehrsson",
+                        email: "goran@technipelago.se", parent: technipelago, title: "Developer", true)
             }
         }
     }
